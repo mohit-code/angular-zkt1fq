45844954d15bba97fc6ce1cebc4f85c6a3330fee
mohit-code <mhtklkrn@gmail.com>
2019-07-26 09:43:43 -0400

    6 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+   onClickSubmit(data) {
+     if(data.Username=='mohit' && data.Password=='mohit'){
+
+     }
+     else{
+       alert("Invalid username or password");
+     }
+     alert("Entered Username:"+data.Username);
+     alert("Entered Password:"+data.Password);
+   }
+   getUrl() {
+    return "url('https://i.pinimg.com/originals/da/d6/5c/dad65c030621548b4f6d9dccbf3b12df.gif')";
+  }
 }
